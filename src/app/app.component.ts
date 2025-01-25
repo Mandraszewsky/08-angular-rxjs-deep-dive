157bcdcfba4cc62bab2ae141450585e805d042d2
Mandraszewsky <mandrah@o2.pl>
2025-01-25 13:55:14 +0100

    observables & signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, DestroyRef, OnInit, inject } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, DestroyRef, OnInit, effect, inject, signal } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +8,33 @@ import { interval } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   private destoryRef = inject(DestroyRef);
+  clickCount = signal(0);
 
+  //signals:
+  constructor(){
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times.`)
+    });
+  }
+
+  //observables:
   ngOnInit(): void {
-    const subscription = interval(1000).subscribe({
+    const subscription = interval(1000).pipe(map((value) => value * 2)).subscribe({
       next: (value) => console.log(value),
     });
 
     this.destoryRef.onDestroy(() => {
       subscription.unsubscribe();
     });
+
+    //signales example for the same thing (also there should be a computed signal that * 2 value):
+    // setInterval(() => {
+    //   this.interval.update(prevInterval => prevInterval + 1);
+    // }, 1000);
+  }
+
+  //signals:
+  onClick(){
+    this.clickCount.update(prevCount => prevCount + 1);
   }
 }
